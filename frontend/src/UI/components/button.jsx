8582f85da5297b397f0aebacde54a30a8bf058f6
Hai Nam <ddhnam21@clc.fitus.edu.vn>
2023-11-30 09:43:45 +0700

    merge init front end w. main

---
+++ frontend/src/UI/components/button.jsx
@@ -0,0 +1,11 @@
+import colors from "../color";
+import Button from '@mui/material/Button';
+
+export default function VTButton(props){
+    const {children} = props;
+    return(
+        <Button>
+            {children}
+        </Button>
+    );
+}
