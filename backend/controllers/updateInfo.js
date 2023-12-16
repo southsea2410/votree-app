d7ae56a9fe0926371c167c4589bab2e6c56eecab
thang0306 <tdoannam980@gmail.com>
2023-12-16 14:26:24 +0700

    Completed updateInfo and updateToSeller

--- backend/controllers/updateInfo.js
+++ backend/controllers/updateInfo.js
@@ -1,28 +1,39 @@
 const User = require('../models/userModel');
+const Seller = require('../models/sellerModel');
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError, NotFoundError } = require('../errors');
-const Token = require("../models/Authentication/Token");
-const crypto = require("crypto");
-const {
-  createTokenUser,
-  attachCookiesToResponse,
-} = require('../utils');
+const Token = require('../models/Authentication/Token');
+const crypto = require('crypto');
+const { createTokenUser, attachCookiesToResponse } = require('../utils');
 
-const getUserInfo = async (req, res) => {
+const getInfo = async (req, res) => {
   const {
-    user: { userId },
+    user: { userId, role },
   } = req;
 
   try {
-    const infoUser = await User.findOne({
-      _id: userId,
-    }).select('fullName dateOfBirth email phone');
+    let userInfo;
 
-    if (!infoUser) {
+    if (role === 'user') {
+      userInfo = await User.findOne({ _id: userId }).select(
+        'role fullName dateOfBirth gender phoneNumber email address interest,',
+      );
+    } else if (role === 'seller') {
+      userInfo = await User.findOne({ _id: userId })
+        .populate(
+          'sellerDetails',
+          'storeName storeLocation storeEmail storePhoneNumber',
+        )
+        .select(
+          'role fullName dateOfBirth gender phoneNumber email address interest',
+        );
+    }
+
+    if (!userInfo) {
       throw new NotFoundError(`No user with id ${userId}`);
     }
 
-    res.status(StatusCodes.OK).json({ infoUser });
+    res.status(StatusCodes.OK).json({ userInfo });
   } catch (error) {
     res
       .status(StatusCodes.INTERNAL_SERVER_ERROR)
@@ -30,38 +41,60 @@ const getUserInfo = async (req, res) => {
   }
 };
 
-const updateUserInfo = async (req, res) => {
+const updateInfo = async (req, res) => {
   const {
-    body: { role },
-    user: { userId },
+    body: {
+      fullName,
+      dateOfBirth,
+      gender,
+      phoneNumber,
+      email,
+      address,
+      interest,
+      storeName,
+      storeLocation,
+      storeEmail,
+      storePhoneNumber,
+    },
+    user: { userId, role },
   } = req;
 
-  const currentRole = User.findOne({ _id: userId }).select('role');
-
   try {
-    const updatedUser = await User.findByIdAndUpdate(
-      { _id: userId },
-      { role },
-      { new: true, runValidators: true },
-    );
+    let updatedUser;
 
-    if (!updatedUser) {
-      throw new NotFoundError(`No user with id ${userId}`);
-    }
+    if (role === 'user') {
+      updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+        new: true,
+        runValidators: true,
+      });
+    } else if (role === 'seller') {
+      updatedUser = await User.findByIdAndUpdate(
+        userId,
+        {
+          fullName,
+          dateOfBirth,
+          gender,
+          phoneNumber,
+          email,
+          address,
+          interest,
+        },
+        { new: true, runValidators: true },
+      );
 
-    const tokenUser = createTokenUser(updatedUser);
-    if (currentRole !== updatedUser.role) {
-      refreshToken = crypto.randomBytes(40).toString('hex');
-      attachCookiesToResponse({ res, user: tokenUser, refreshToken });
-      // update Token in database
-      await Token.findOneAndUpdate(
-        { user: userId },
-        { refreshToken },
+      await Seller.findOneAndUpdate(
+        { _id: updatedUser.sellerDetails },
+        { storeName, storeLocation, storeEmail, storePhoneNumber },
         { new: true, runValidators: true },
       );
     }
-    res.status(StatusCodes.OK).json({ user: tokenUser });
 
+    if (!updatedUser) {
+      // User not found
+      throw new NotFoundError(`No user with id ${userId}`);
+    }
+
+    res.status(StatusCodes.OK).json({ user: updatedUser });
   } catch (error) {
     if (error.name === 'ValidationError') {
       res.status(StatusCodes.BAD_REQUEST).json({ error: error.message });
@@ -73,7 +106,58 @@ const updateUserInfo = async (req, res) => {
   }
 };
 
+const updateToSeller = async (req, res) => {
+  const {
+    body: { storeName, storeLocation, storeEmail, storePhoneNumber },
+    user: { userId },
+  } = req;
+
+  try {
+    const currentUser = await User.findOne({ _id: userId, role: 'user' });
+
+    if (!currentUser) {
+      return res.status(StatusCodes.NOT_FOUND).json({
+        error: 'User not found or is not eligible for role update.',
+      });
+    }
+
+    const seller = new Seller({
+      storeName,
+      storeLocation,
+      storeEmail,
+      storePhoneNumber,
+    });
+
+    await seller.save();
+
+    await User.findByIdAndUpdate(userId, {
+      role: 'seller',
+      sellerDetails: seller._id,
+    });
+
+    const updatedUser = await User.findById(userId).populate('sellerDetails');
+
+    const tokenUser = createTokenUser(updatedUser);
+    if (updatedUser.role !== currentUser.role) {
+      refreshToken = crypto.randomBytes(40).toString('hex');
+      attachCookiesToResponse({ res, user: tokenUser, refreshToken });
+      await Token.findOneAndUpdate(
+        { user: userId },
+        { refreshToken },
+        { new: true, runValidators: true },
+      );
+    }
+
+    res.status(StatusCodes.OK).json({ user: updatedUser });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: error.message });
+  }
+};
+
 module.exports = {
-  getUserInfo,
-  updateUserInfo,
+  getInfo,
+  updateInfo,
+  updateToSeller,
 };
--- backend/utils/checkIndividualPermissions.js
+++ backend/utils/checkIndividualPermissions.js
@@ -4,11 +4,11 @@ const checkIndividualPermissions = (requestUser, resourceUserId) => {
   // console.log(requestUser);
   // console.log(resourceUserId);
   // console.log(typeof resourceUserId);
-  
+
   // if (requestUser.role === 'admin') return;
   if (requestUser.userId === resourceUserId.toString()) return;
   throw new CustomError.UnauthorizedError(
-    'Not authorized to access this route'
+    'Not authorized to access this route',
   );
 };
 
