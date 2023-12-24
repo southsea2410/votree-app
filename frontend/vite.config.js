695584651b8c77af0986235fcd8777b8eb62e169
Hai Nam <ddhnam21@clc.fitus.edu.vn>
2023-12-24 10:18:48 +0700

    Fix routing OrderProduct

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'https://app-vniii7ofca-uc.a.run.app',
+                target: 'http://localhost:3000',
                 changeOrigin: true
             }
         }
